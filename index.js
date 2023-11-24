85c1e56e3514835abb9c704d402d0a0f1be9ba30
Adrian Suarez <asuarez9382@gmail.com>
2023-11-24 00:32:09 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeArr) {
+    const employee = {
+        firstName: employeeArr[0],
+        familyName: employeeArr[1],
+        title: employeeArr[2],
+        payPerHour: employeeArr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(inputArrs) {
+    const newRecord = inputArrs.map(createEmployeeRecord)
+    return newRecord
+}
+
+
+function createTimeInEvent(dateStamp) {
+    const [datePart, timePart] = dateStamp.split(' ');
+    const hours = parseInt(timePart.substring(0, 4), 10);
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: hours,
+        date: datePart
+    }
+    this.timeInEvents.push(timeInEvent)
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [datePart, timePart] = dateStamp.split(' ');
+    const hours = parseInt(timePart.substring(0, 4), 10);
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: hours,
+        date: datePart
+    }
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === dateStamp);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === dateStamp);
+    
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp)
+    const wages = hours* this.payPerHour
+    return wages
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    const foundEmployee = srcArray.find(employee => employee.firstName === firstName )
+    return foundEmployee
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -9,7 +72,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +83,19 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
+
+
+
+function calculatePayroll(employeeRecords) {
+    const totalPayroll = employeeRecords.reduce((acc, employeeRecord) => {
+      const totalWages = allWagesFor.call(employeeRecord);
+  
+      return acc + totalWages;
+    }, 0);
+  
+    return totalPayroll;
+  }
+
 
